2f00f4f08289194635373a6c6a689e0a4669386f
andrewmustain-commits <andrewmustain@safetyassuranceglobal.com>
2026-08-29 13:16:51 -0700

    WEB v18: centralize public brand asset paths

---
+++ src/config/brand-assets.ts
@@ -0,0 +1,9 @@
+export const brandAssets = {
+  sagLogo: '/images/brand/sag-logo-v2.svg',
+  sagLogoDark: '/images/brand/sag-logo-dark.svg',
+  sagLogoWhite: '/images/brand/sag-logo-white.svg',
+  sagIcon: '/images/brand/sag-icon.svg',
+  instituteCrest: '/images/brand/institute-crest.svg'
+} as const;
+
+export const brandAttribution = 'Powered by Mandavere';
